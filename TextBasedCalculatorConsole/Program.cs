79a51d3e0de803df42010bd7f153b5067e4808f7
orenkup <orenwantsyou@gmail.com>
2022-04-17 04:26:38 +0300

    Add files via upload

---
+++ TextBasedCalculatorConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TextBaseCalculator;
+Console.WriteLine("Welcome to Oren's Text Based Calculator!");
+Console.WriteLine("Basic rules: the calculator assums that the input is a valid expression, for example 'x = 1 + 3'");
+Console.WriteLine("Add as many expressions as you want. when you want to calculate, write 'calc'");
+
+List<string> inputs = new List<string>();
+
+while (true)
+{
+    Console.Write(">> ");
+    string input = Console.ReadLine();
+
+    inputs.Add(input);
+    if (input == "calc")
+    {
+        
+        var output = new Calculator().Calculate(inputs);
+
+        foreach (var item in output)
+        {
+            Console.WriteLine(item.Key + ":" + item.Value);
+        }
+    }
+}
+
